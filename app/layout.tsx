45f17da09b8e78923691e7c58da662e4e96bb071
pitsillides <240495332+pitsillides@users.noreply.github.com>
2026-06-04 16:22:32 +0300

    Replace Font Awesome CDN with local icon subset

--- app/layout.tsx
+++ app/layout.tsx
@@ -114,14 +114,6 @@ export default function RootLayout({
   return (
     <html lang="el-CY">
       <head>
-        <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-          integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
-          crossOrigin="anonymous"
-          referrerPolicy="no-referrer"
-        />
         <meta name="theme-color" content="#12133c" />
       </head>
       <body className="min-h-screen bg-slate-50 antialiased overflow-x-hidden">
